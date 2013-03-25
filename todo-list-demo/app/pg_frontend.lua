bc0804cff4e5e7314abf90a180bd53480953aa1b
Fengyuan Chen <cfy1990@gmail.com>
2013-03-25 10:01:23 +0800

    已将数据库交互部分和网页后端代码分离

--- todo-list-demo/app/pg_frontend.lua
+++ todo-list-demo/app/pg_frontend.lua
@@ -1,4 +1,4 @@
-module("todo-list-api", package.seeall)
+module("pg_frontend", package.seeall)
 
 local JSON = require("cjson")
 local utils = require("awutils")
@@ -6,15 +6,6 @@ local html = require("htmlentities")
 local entities = html.htmlentities
 local rds_parser = require "rds.parser"
 local quote_pgsql_str = ndk.set_var.set_quote_pgsql_str
-
-function get_arg_html_entities_encoded(req,name,default)
-   local arg = req:get_arg(name)
-   if arg then
-      return entities(arg)
-   else
-      return default
-   end
-end
 
 function pg_conn(str)
    local pg_res = ngx.location.capture("/postgres_default",{vars = {internal_postgres_sql = str}})
@@ -48,4 +39,33 @@ end
 
 function get_item(id)
    return pg_conn_format("select * from tl_items where item_id = %s limit 1",quote_pgsql_str(id)).resultset
+end
+
+function count_items()
+   return resp:writeln(pg_conn("select count(1) from tl_items").resultset[1].count)
+end
+
+function create_item(title,state,content)
+   state = quote_pgsql_str(state)
+   title = quote_pgsql_str(title)
+   content = quote_pgsql_str(content)
+   return pg_conn_format("insert into tl_items (item_todo_state,item_title,item_content) values(%s,%s,%s)",state,title,content)
+end
+
+function get_item(id)
+   local r = pg_conn_format("select * from tl_items where item_id = %s limit 1",quote_pgsql_str(id))
+   if (#r.resultset == 0 ) then
+      return nil
+   else
+      return r.resultset
+   end
+end
+
+function update_item(id,state,title,content)
+   local q_id,q_state,q_title,q_content = quote_pgsql_str(id),quote_pgsql_str(state),quote_pgsql_str(title),quote_pgsql_str(content)
+   return pg_conn_format("update tl_items set item_todo_state = %s,item_title = %s,item_content = %s where item_id = %s",q_state,q_title,q_content,q_id)
+end
+
+function delete_item(id)
+   return pg_conn_format("delete from tl_items where item_id = %s",quote_pgsql_str(id))
 end
--- todo-list-demo/app/todo-list-public-api.lua
+++ todo-list-demo/app/todo-list-public-api.lua
@@ -3,44 +3,51 @@ module("todo-list-public-api", package.seeall)
 local JSON = require("cjson")
 local Redis = require("resty.redis")
 local utils = require("awutils")
-local tl = require("todo-list-api")
+local pg = require("pg_frontend")
 local html = require("htmlentities")
 local entities = html.htmlentities
 local rds_parser = require "rds.parser"
-local pg_conn = tl.pg_conn
-local pg_conn_format = tl.pg_conn_format
-local quote_pgsql_str = ndk.set_var.set_quote_pgsql_str
-local get_arg_html_entities_encoded = tl.get_arg_html_entities_encoded
+
+local db = pg
+
+function get_arg_html_entities_encoded(req,name,default)
+   local arg = req:get_arg(name)
+   if arg then
+      return entities(arg)
+   else
+      return default
+   end
+end
 
 function count_items(req,resp)
-   resp:writeln(pg_conn("select count(1) from tl_items").resultset[1].count)
+   return db.count_items()
 end
 
 function get_last_item_html(req,resp)
-   resp:ltp("item.html",{todo_lists = tl.get_last_item()})
+   resp:ltp("item.html",{todo_lists = db.get_last_item()})
 end
 
 function get_all_items_html(req,resp)
-   resp:ltp("item.html",{todo_lists = tl.get_all_items()})
+   resp:ltp("item.html",{todo_lists = db.get_all_items()})
 end
 
 function get_item_html(req,resp)
    if req.method=='POST' then req:read_body() end
    local id = req:get_arg("id")
    if (not id) then
-      resp:ltp("item.html",{todo_lists = tl.get_last_item()})
+      resp:ltp("item.html",{todo_lists = db.get_last_item()})
    else
-      resp:ltp("item.html",{todo_lists = tl.get_item(id)})
+      resp:ltp("item.html",{todo_lists = db.get_item(id)})
    end
 end
    
 function create_item_json(req,resp)
    if req.method=='POST' then req:read_body() end
-   local state = quote_pgsql_str(req:get_arg("state","0"))
-   local title = quote_pgsql_str(req:get_arg("title","no title"))
-   local content = quote_pgsql_str(req:get_arg("content","no content"))
+   local title = req:get_arg("title","no title")
+   local state = req:get_arg("state","0")
+   local content = req:get_arg("content","no content")
    resp.headers['Content-Type'] = 'application/json'
-   resp:writeln(JSON.encode(pg_conn_format("insert into tl_items (item_todo_state,item_title,item_content) values(%s,%s,%s)",state,title,content)));
+   resp:writeln(JSON.encode(db.create_item(title,state,content)));
 end
 
 function get_item_json(req,resp)
@@ -50,16 +57,17 @@ function get_item_json(req,resp)
    if (not id) then
       resp:writeln(JSON.encode({err="id required"}))
       logger:w("uri: %s ;err: %s",req.uri,"id required")
-      return
+      return nil
    end
-   local r = pg_conn_format("select * from tl_items where item_id = %s limit 1",quote_pgsql_str(id))
-   if (#r.resultset == 0) then
+   local data = db.get_item(id)
+   if (not data) then
       resp:writeln(JSON.encode({err="can't find id: " .. id}))
       logger:w("uri: %s ;err: %s",req.uri,"can't find id: " ..id)
    else
-      resp:writeln(JSON.encode(r.resultset))
+      resp:writeln(JSON.encode(data))
    end
 end
+
 function update_item_json(req,resp)
    if req.method=='POST' then req:read_body() end
    local id = req:get_arg("id")
@@ -68,13 +76,13 @@ function update_item_json(req,resp)
       logger:w("uri: %s ;err: %s",req.uri,"id required")
       return
    end
-   local old = pg_conn_format("select * from tl_items where item_id = %s limit 1",quote_pgsql_str(id)).resultset[1]
+   local old = db.get_item(id)[1]
    local state = get_arg_html_entities_encoded(req,"state",old.item_todo_state)
    local title = get_arg_html_entities_encoded(req,"title",old.item_title)
    local content = get_arg_html_entities_encoded(req,"content",old.item_content)
-   local q_id,q_state,q_title,q_content = quote_pgsql_str(id),quote_pgsql_str(state),quote_pgsql_str(title),quote_pgsql_str(content)
+   
    resp.headers['Content-Type'] = 'application/json'
-   resp:writeln(JSON.encode(pg_conn_format("update tl_items set item_todo_state = %s,item_title = %s,item_content = %s where item_id = %s",q_state,q_title,q_content,q_id)))
+   resp:writeln(JSON.encode(db.update_item(id,state,title,content)))
 end
 
 function delete_item_json(req,resp)
@@ -86,5 +94,5 @@ function delete_item_json(req,resp)
       return
    end
    resp.headers['Content-Type'] = 'application/json'
-   resp:writeln(JSON.encode(pg_conn_format("delete from tl_items where item_id = %s",quote_pgsql_str(id))))
+   resp:writeln(JSON.encode(db.delete_item(id)))
 end
